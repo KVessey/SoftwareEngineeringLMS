54b31c57e7d8f7be973a842d3e0a4e6cf085e1ac
Brandon Popson <Brandon.Popson@outlook.com>
2018-11-04 13:32:16 -0600

    Created scaffolding for Student, Enrollment, and Course

---
+++ src/LearningManagementSystem/db/migrate/20181104193103_create_enrollments.rb
@@ -0,0 +1,11 @@
+class CreateEnrollments < ActiveRecord::Migration[5.2]
+  def change
+    create_table :enrollments do |t|
+      t.integer :student_id
+      t.string :semester
+      t.integer :course_id
+
+      t.timestamps
+    end
+  end
+end
