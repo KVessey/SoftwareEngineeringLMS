2013cdcd36ccdde013e821ef698069d4e694fb92
Brandon Popson <Brandon.Popson@outlook.com>
2018-11-14 15:03:07 -0600

    Add full_name attribute to students

--- src/LearningManagementSystem/app/models/student.rb
+++ src/LearningManagementSystem/app/models/student.rb
@@ -5,4 +5,8 @@ class Student < ApplicationRecord
 
     validates_length_of :first_name, :within => 1..20
     validates_length_of :last_name, :within => 1..20
+
+    def full_name
+        "#{last_name}, #{first_name}"
+      end
 end
