3f44ddc854cbada0e02292222827145025776fd0
Brandon Popson <Brandon.Popson@outlook.com>
2018-11-18 21:43:33 -0600

    Sort students by last_name on students controller

--- src/LearningManagementSystem/app/controllers/students_controller.rb
+++ src/LearningManagementSystem/app/controllers/students_controller.rb
@@ -4,7 +4,7 @@ class StudentsController < ApplicationController
   # GET /students
   # GET /students.json
   def index
-    @students = Student.all
+    @students = Student.all.reorder('last_name ASC')
   end
 
   # GET /students/1
